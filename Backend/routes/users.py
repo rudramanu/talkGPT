9615cb82f423f54d0780416fb8f6e8baf17d2b40
Rudra <merudramanu@gmail.com>
2023-07-20 08:36:38 +0530

    added navbar login signup

--- Backend/routes/users.py
+++ Backend/routes/users.py
@@ -1,14 +1,14 @@
 
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
-from backend.models.users import User
-
+from models.users import User
+from dotenv import load_dotenv
 from flask_cors import CORS
 
 from flask_pymongo import PyMongo
 
 users_bp = Blueprint('users', __name__)
-users_bp.config['MONGO_URI'] = "mongodb+srv://rudramanu:rudramanu@cluster0.l4qfum1.mongodb.net/talkGPT?retryWrites=true&w=majority"
+# users_bp.config['MONGO_URI'] = load_dotenv
 mongo = PyMongo(users_bp)
 CORS(users_bp)
 
@@ -20,9 +20,8 @@ def register():
     name = data.get("name")
     email = data.get("email")
     password = data.get("password")
-    image = data.get("image")
 
-    if not name or not email or not password or not image:
+    if not name or not email or not password:
         return jsonify({"message": "All details are required"}), 400
 
     user = User.query.filter_by(email=email).first()
@@ -30,7 +29,7 @@ def register():
         return jsonify({"message": "User already registered"}), 400
 
     password_hash = generate_password_hash(password, method='bcrypt', salt_length=3)
-    new_user = User(name=name, email=email, password=password_hash, image=image)
+    new_user = User(name=name, email=email, password=password_hash)
     new_user.save()
 
     return jsonify({"message": "User Registered"}), 200
---
+++ Frontend/gpt/src/app/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent {
+  // Add your logout logic here
+  logout(): void {
+    // Perform logout actions
+  }
+}
